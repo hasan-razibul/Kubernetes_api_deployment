77ea496abd529e977cb1e54092d4bae8a1e3ba57
Razibul Hasan <razhasan93@gmail.com>
2024-03-22 19:43:00 +0000

    Kubernetes fix (#1)
    * minikube works
    
    * local and minikube works

--- api/database.py
+++ api/database.py
@@ -6,8 +6,12 @@ import os
 
 db_username = os.environ.get('DB_USERNAME')
 db_password = os.environ.get('DB_PASSWORD')
+ENVIRONAMENT = os.environ.get("ENV")
 
-DATABASE_URL = f"postgresql://{db_username}:{db_password}@db/postgres"
+if ENVIRONAMENT == "local":
+    DATABASE_URL = f"postgresql://{db_username}:{db_password}@db/postgres"
+if ENVIRONAMENT == "kubernetes":
+    DATABASE_URL = f"postgresql://{db_username}:{db_password}@postgres-service/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
